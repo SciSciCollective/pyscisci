8df07b794d83850bf7677db4e4dd4a1beb9fdd98
Alex Gates <ajgates42@gmail.com>
2020-05-07 19:38:36 -0400

    Update __init__.py

--- pyscisci/__init__.py
+++ pyscisci/__init__.py
@@ -8,8 +8,8 @@ __author__ = """\n""".join([
     'Alexander J Gates <ajgates42@gmail.com>'
 ])
 
-__version__ = '0.1.3'
-__release__ = '0.1.2'
+__version__ = '0.1.4'
+__release__ = '0.1'
 
 #from .analysis import *
 #from .bibnetwork import *
