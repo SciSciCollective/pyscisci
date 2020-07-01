aff3f7a90853af2b7664b6bb7d9a1f60f4773e53
Alex Gates <ajgates42@gmail.com>
2020-07-01 19:06:25 -0400

    temporal raostirling

--- pyscisci/metrics.py
+++ pyscisci/metrics.py
@@ -341,17 +341,8 @@ def compute_cnorm(pub2ref, pub2year):
             if cnorm.get(citedpid, None) is None:
                 cnorm[citedpid] = {y:year_cites/np.mean()}
 
-###
-def RaoStirling(data_array, distance_matrix):
-    rs = 0.0
-    normed_data = data_array / data_array.sum()
-    for ip, jp in itertools.combinations(np.nonzero(data_array)[0], 2):
-        rs += distance_matrix[ip, jp] * normed_data[ip] * normed_data[jp]
-
-    return rs
-
 def compute_raostriling_interdisciplinarity(pub2ref_df, pub2field_df, focus_pub_ids=None, pub2field_norm=True, temporal=False,
-    citation_direction='references', field_distance_metric='cosine', precomputed_distance_matrix=None, show_progress=False):
+    citation_direction='references', field_distance_metric='cosine', distance_matrix=None, show_progress=False):
     """
     Calculate the RaoStirling index as a measure of a publication's interdisciplinarity.
     See :cite:`stirling20` for the definition and :cite:`gates2019naturereach` for an application.
@@ -382,7 +373,7 @@ def compute_raostriling_interdisciplinarity(pub2ref_df, pub2field_df, focus_pub_
         The interfield distance metric.  Valid entries come from sklearn.metrics.pairwise_distances:
         ‘cosine‘, ‘euclidean’, ‘l1’, ‘l2’, etc.
 
-    :param precomputed_distance_matrix : numpy array, default None
+    :param distance_matrix : numpy array, default None
         The precomputed field distance matrix.
 
     :param show_progress : bool, default False
@@ -408,10 +399,17 @@ def compute_raostriling_interdisciplinarity(pub2ref_df, pub2field_df, focus_pub_
     pub2field_df['FieldId'] = [field2int[fid] for fid in pub2field_df['FieldId'].values]
     Nfields = len(field2int)
 
+    if temporal:
+        years = np.sort(pub2ref_df['CitingYear'].unique())
+        year2int = {y:i for i, y in enumerate(years)}
+        Nyears = years.shape[0]
+
     # check that the precomputed distance matrix is the correct size
     if not precomputed_distance_matrix is None:
-        if precomputed_distance_matrix != (Nfields, Nfields):
+        if not temporal and precomputed_distance_matrix != (Nfields, Nfields):
             raise pySciSciMetricError('The precomputed_distance_matrix is of the wrong size to compute the RaoStirling interdisciplinarity for the publications passed.')
+        elif temporal and precomputed_distance_matrix != (Nyears, Nfields, Nfields):
+            raise pySciSciMetricError('The precomputed_distance_matrix is of the wrong size to compute the RaoStirling interdisciplinarity for the publications and years passed.')
 
     # the assignment of a publication to a field is 1/(number of fields) when normalized, and 1 otherwise
     if pub2field_norm:
@@ -434,7 +432,7 @@ def compute_raostriling_interdisciplinarity(pub2ref_df, pub2field_df, focus_pub_
     del pub2ref_df['PublicationId']
 
     # we need to calcuate the field 2 field distance matrix
-    if precomputed_distance_matrix is None:
+    if distance_matrix is None:
 
         # merge the references to the fields for the source fields
         pub2ref_df = pub2ref_df.merge(pub2field_df, how='left', left_on='SourceId', right_on='PublicationId').rename(
@@ -451,44 +449,88 @@ def compute_raostriling_interdisciplinarity(pub2ref_df, pub2field_df, focus_pub_
         # is the product of the source and target contributions
         pub2ref_df['SourcePubFieldContribution'] = pub2ref_df['SourcePubFieldContribution'] * pub2ref_df['TargetPubFieldContribution']
 
-        #print(pub2ref_df)
-        # calculate the field representation vectors
-        field2field_mat = dataframe2bipartite(df=pub2ref_df, rowname='SourceFieldId', colname='TargetFieldId',
-            shape=(Nfields, Nfields), weightname='SourcePubFieldContribution').todense()
+        # differeniate between the temporal and the static RS
+        if temporal:
+            # make the temporal distance matrix
+            distance_matrix = np.zeros((Nyears, Nfields, Nfields))
 
-        # we no longer need the 'SourceFieldId' or 'SourcePubFieldContribution'
+            for y, ydf in pub2ref_df.groupby('CitingYear'):
+                # calculate the field representation vectors for this year only
+                yfield2field_mat = dataframe2bipartite(df=ydf, rowname='SourceFieldId', colname='TargetFieldId',
+                        shape=(Nfields, Nfields), weightname='SourcePubFieldContribution')
+
+                # now compute the distance matrix for this year only
+                distance_matrix[year2int[y]] = pairwise_distances(yfield2field_mat, metric=field_distance_metric)
+
+        else:
+            # calculate the field representation vectors
+            field2field_mat = dataframe2bipartite(df=pub2ref_df, rowname='SourceFieldId', colname='TargetFieldId',
+                shape=(Nfields, Nfields), weightname='SourcePubFieldContribution')
+
+            # now compute the distance matrix
+            distance_matrix = pairwise_distances(field2field_mat, metric=field_distance_metric)
+
+        # we no longer need the 'SourceFieldId' or 'SourcePubFieldContribution' so cleanup
         del pub2ref_df['SourceFieldId']
         del pub2ref_df['SourcePubFieldContribution']
         pub2ref_df.drop_duplicates(subset=['SourceId', 'TargetId', 'TargetFieldId'], inplace=True)
 
-        # now compute the distance matrix
-        precomputed_distance_matrix = pairwise_distances(field2field_mat, metric=field_distance_metric)
 
     # Now we start on the RaoStiring calculation
-    precomputed_distance_matrix = spsparse.csr_matrix(precomputed_distance_matrix)
 
-    # now we need to map individual publications to the rows of our matrix
-    pub2int = {pid:i for i, pid in enumerate(np.sort(pub2ref_df['SourceId'].unique()))}
-    pub2ref_df['SourceId'] = [pub2int[fid] for fid in pub2ref_df['SourceId'].values]
-    Npubs = len(pub2int)
-
-    # drop any citation relationships for which we dont have field information and map the ids to int
+    # drop any citation relationships for which we dont have field information
     pub2ref_df.dropna(inplace=True)
-    pub2ref_df[['SourceId', 'TargetFieldId']] = pub2ref_df[['SourceId', 'TargetFieldId']].astype(int)
 
-    # calculate the publication representation vectors over fields
-    pub2field_mat = dataframe2bipartite(df=pub2ref_df, rowname='SourceId', colname='TargetFieldId',
-            shape=(Npubs, Nfields), weightname='TargetPubFieldContribution').tocsr()
+    if temporal:
 
-    # make sure the publication 2 field vector is normalized
-    pub2field_mat = normalize(pub2field_mat, norm='l1', axis=1)
+        rsdf = []
+        for y, ydf in pub2ref_df.groupby('CitingYear'):
 
-    # finally, we calculate the matrix representation of the RS measure
-    rsdf = 0.5 * np.squeeze(np.asarray(pub2field_mat.dot(precomputed_distance_matrix).multiply(pub2field_mat).sum(axis=1)))
+            # for each year, we need to map individual publications to the rows of our matrix
+            ypub2int = {pid:i for i, pid in enumerate(np.sort(ydf['SourceId'].unique()))}
+            ydf['SourceId'] = [ypub2int[fid] for fid in ydf['SourceId'].values]
+            ydf[['SourceId', 'TargetFieldId']] = ydf[['SourceId', 'TargetFieldId']].astype(int)
+            yNpubs = len(ypub2int)
 
-    rsdf = pd.DataFrame(zip(np.sort(pub2ref_df['SourceId'].unique()), rsdf), columns = ['PublicationId', 'RaoStirling'])
+            # calculate the publication representation vectors over fields
+            ypub2field_mat = dataframe2bipartite(df=ydf, rowname='SourceId', colname='TargetFieldId',
+                shape=(yNpubs, Nfields), weightname='TargetPubFieldContribution').tocsr()
 
-    return rsdf, precomputed_distance_matrix, field2int
+            # make sure the publication 2 field vector is normalized
+            ypub2field_mat = normalize(ypub2field_mat, norm='l1', axis=1)
+
+            # finally, we calculate the matrix representation of the RS measure
+            yrsdf = 0.5 * np.squeeze(np.asarray(ypub2field_mat.dot(distance_matrix[year2int[y]]).multiply(ypub2field_mat).sum(axis=1)))
+
+            rsdf.append(pd.DataFrame(zip(np.sort(ydf['SourceId'].unique()), yrsdf, [y]*yNpubs), columns = ['PublicationId', 'RaoStirling', 'CitingYear']))
+
+        rsdf = pd.concat(rsdf)
+
+        return rsdf, precomputed_distance_matrix, field2int, years
+
+    else:
+
+        # first map individual publications to the rows of our matrix
+        pub2int = {pid:i for i, pid in enumerate(np.sort(pub2ref_df['SourceId'].unique()))}
+        pub2ref_df['SourceId'] = [pub2int[fid] for fid in pub2ref_df['SourceId'].values]
+        pub2ref_df[['SourceId', 'TargetFieldId']] = pub2ref_df[['SourceId', 'TargetFieldId']].astype(int)
+        Npubs = len(pub2int)
+
+        # calculate the publication representation vectors over fields
+        pub2field_mat = dataframe2bipartite(df=pub2ref_df, rowname='SourceId', colname='TargetFieldId',
+                shape=(Npubs, Nfields), weightname='TargetPubFieldContribution').tocsr()
+
+        # make sure the publication 2 field vector is normalized
+        pub2field_mat = normalize(pub2field_mat, norm='l1', axis=1)
+
+        # finally, we calculate the matrix representation of the RS measure
+        rsdf = 0.5 * np.squeeze(np.asarray(pub2field_mat.dot(distance_matrix).multiply(pub2field_mat).sum(axis=1)))
+
+        rsdf = pd.DataFrame(zip(np.sort(pub2ref_df['SourceId'].unique()), rsdf), columns = ['PublicationId', 'RaoStirling'])
+
+        return rsdf, distance_matrix, field2int
+
+
 
 ### Novelty
 
