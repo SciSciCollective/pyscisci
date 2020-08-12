5ba4e2894ef06e659bfee14d08dc9c03e4f8f72d
Alexander Gates <ajgates42@gmail.com>
2020-08-12 19:44:14 -0400

    Update metrics.py

--- pyscisci/metrics.py
+++ pyscisci/metrics.py
@@ -247,7 +247,7 @@ def pub_credit_share(focus_pid, pub2ref_df, pub2author_df, temporal=False, norme
     elif focus_authors.shape[0] == 1:
         if temporal:
             years = np.sort(pub2ref_df.loc[pub2ref_df['CitedPublicationId'] == focus_pid]['CitingYear'].unique())
-            return np.ones(shape=(1,years.shape[0]), dype=float), author2int, years
+            return np.ones(shape=(1,years.shape[0])), author2int, years
         else:
             return np.array([1.0]), author2int
 
