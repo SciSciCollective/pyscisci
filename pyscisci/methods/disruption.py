bab05f52475cd108a8d821a0b32a32b028409d9e
Alexander Gates <ajgates42@gmail.com>
2023-01-10 08:46:28 -0500

    add windowed disruption

--- pyscisci/methods/disruption.py
+++ pyscisci/methods/disruption.py
@@ -16,21 +16,60 @@ else:
     from tqdm import tqdm
 
 ### Disruption
-def disruption_index(pub2ref, focus_pub_ids = None, show_progress=False):
+def disruption_index(pub2ref, focus_pub_ids = None, cite_window = None, ref_window = None, show_progress=False):
     """
     Calculate the disruption index as first proposed in :cite:`Funk2017disrupt` and used in :cite:`Wu2019teamsdisrupt`.
+    We also include the windowed disruption index used in :cite:`Park2023timedisrupt`.
 
-    
+    Parameters
+    ----------
+
+    :param pub2ref : DataFrame
+        A DataFrame with the citation information for each Publication.
+
+    :param focus_pub_ids : numpy array
+        A subset of publication ids to focus on for the disruption index.
+
+    :param cite_window : list of two ints, default None
+        If None, no citation window is applied.
+        If [l, u] where, l,u are ints, then only citations whose year difference is greater than or equal to the lower bound l and 
+            less than or equal to the upper bound u are used.  e.g. [0,5] uses citations within 5 years of publication (and not before).
+
+    :param ref_window : list of two ints, default None
+        If None, no reference window is applied.
+        If [l, u] where, l,u are ints, then only references whose year difference is greater than or equal to the lower bound l and 
+            less than or equal to the upper bound u are used.  e.g. [0,5] uses references within 5 years of publication (and not after).
+
+    show_progress : bool, default False
+        Show calculation progress. 
+
+    Returns
+    -------
+    disruption : DataFrame
+        A DataFrame with the disruption index for all (cited) publications or publications from the focus_pub_ids list.
+
 
     """
     if show_progress:
         print("Starting computation of disruption index.")
 
-    if focus_pub_ids is None:
-        focus_pub_ids = pub2ref['CitedPublicationId'].unique()
+    if ref_window is None:
+        reference_groups = pub2ref.groupby('CitingPublicationId', sort = False)['CitedPublicationId']
+    else:
+        ref_sub = [ ((y1-y2) >= ref_window[0] and (y1-y2) <=ref_window[1]) for y1,y2 in pub2ref[['CitingYear', 'CitedYear']].values]
+        reference_groups = pub2ref.loc[ref_sub].groupby('CitingPublicationId', sort = False)['CitedPublicationId']
 
-    reference_groups = pub2ref.groupby('CitingPublicationId', sort = False)['CitedPublicationId']
-    citation_groups = pub2ref.groupby('CitedPublicationId', sort = False)['CitingPublicationId']
+    if cite_window is None:
+        citation_groups = pub2ref.groupby('CitedPublicationId', sort = False)['CitingPublicationId']
+    else:
+        cite_sub = [ ((y1-y2) >= cite_window[0] and (y1-y2) <=cite_window[1]) for y1,y2 in pub2ref[['CitingYear', 'CitedYear']].values]
+        citation_groups = pub2ref.loc[cite_sub].groupby('CitingPublicationId', sort = False)['CitedPublicationId']
+
+    if focus_pub_ids is None:
+        if cite_window is None:
+            focus_pub_ids = pub2ref['CitedPublicationId'].unique()
+        else:
+            focus_pub_ids = pub2ref.loc[cite_sub]['CitedPublicationId'].unique()
 
     def get_citation_groups(pid):
         try:
