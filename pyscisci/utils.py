ebd17686671a7a1123ec6e307a84a122ef36ce12
Alexander Gates <ajgates42@gmail.com>
2023-05-17 15:34:04 -0400

    add gini coefficient

--- pyscisci/utils.py
+++ pyscisci/utils.py
@@ -418,6 +418,18 @@ def holder_mean(a, rho=1):
     """
     return (a**rho).sum()**(1.0/rho) / a.shape[0]
 
+def gini(array):
+    """Calculate the Gini coefficient of a numpy array."""
+    # based on bottom eq: http://www.statsdirect.com/help/content/image/stat0206_wmf.gif
+    # from: http://www.statsdirect.com/help/default.htm#nonparametric_methods/gini.htm
+    array = array.flatten() #all values are treated equally, arrays must be 1d
+    
+    array += 0.0000001 #values cannot be 0
+    array = np.sort(array) #values must be sorted
+    index = np.arange(1,array.shape[0]+1) #index per array element
+    n = array.shape[0]#number of array elements
+    return ((np.sum((2 * index - n  - 1) * array)) / (n * np.sum(array))) #Gini coefficient
+
 def simpson(p):
     """
     Simpson diversity
